f1d6c6f3fe0944d0c9f292cf73071629271b10cf
adi <adityaadi8688@gmail.com>
2023-02-12 16:59:52 +0530

    Modified input fields in "Game".

--- src/Game.java
+++ src/Game.java
@@ -5,20 +5,46 @@ public class Game extends DisplayBoard{
             displayBoard(upDown, cards);
             System.out.println("Enter co-oridinate 1");
             String g1 = s.next();
-            int g1x = Integer.valueOf(g1.substring(0, 1))-1;
-            int g1y = Integer.valueOf(g1.substring(1, 2))-1;
-            System.out.println(cards[g1x][g1y]);
+            Integer ch = Integer.valueOf(g1);
+            int g1x=0, g1y=0;
+            if((g1.length()==2)&&ch>0) {
+                g1x = Integer.valueOf(g1.substring(0, 1)) - 1;
+                g1y = Integer.valueOf(g1.substring(1, 2)) - 1;
+                if((g1x>=0&&g1x<=3)&&(g1y>=0&&g1y<=3))
+                    System.out.println(cards[g1x][g1y]);
+                else {
+                    System.out.println("Entered coordinates must be in the range of 1 to 4");
+                    game(upDown, cards);
+                }
+            }else{
+                System.out.println("Enter valid coordinates and positve numbers, Example: 11 = Card 1 and 22 = Card 6");
+                game(upDown,cards);
+            }
 
             System.out.println("Enter co-oridinate 2");
             String g2 = s.next();
-            int g2x = Integer.valueOf(g2.substring(0, 1))-1;
-            int g2y = Integer.valueOf(g2.substring(1, 2))-1;
-            System.out.println(cards[g2x][g2y]);
-            if (cards[g1x][g1y] == cards[g2x][g2y]) {
-                System.out.println("You found a match");
+            ch = Integer.valueOf(g2);
+            int g2x=0,g2y=0;
+            if((g2.length()==2)&&ch>0) {
+                g2x = Integer.valueOf(g2.substring(0, 1)) - 1;
+                g2y = Integer.valueOf(g2.substring(1, 2)) - 1;
+                if ((g2x >= 0 && g2x <= 3) && (g2y >= 0 && g2y <= 3))
+                    System.out.println(cards[g2x][g2y]);
+                else {
+                    System.out.println("Entered coordinates must be in the range of 1 to 4");
+                    game(upDown, cards);
+                }
+            }else {
+                System.out.println("Enter valid coordinates and positve numbers, Example: 11 = Card 1 and 22 = Card 6");
+                game(upDown, cards);
+            }
+            if ((cards[g1x][g1y] == cards[g2x][g2y])&&(!(g1y==g2x&&g1y==g2y))) {
+                System.out.println("Hurray...! You found a match :) ");
                 upDown[g1x][g1y] = true;
                 upDown[g2x][g2y] = true;
                 noDownCards -= 2;
+            }else{
+                System.out.println("Sorry, you have not found a match. please continue.\n");
             }
         }
         displayBoard(upDown, cards);
